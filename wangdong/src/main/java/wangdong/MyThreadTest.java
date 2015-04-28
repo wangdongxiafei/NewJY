51d2d90892ffb362303f5839f6475cbafff2f784
wangdong <626091338@qq.com>
2015-04-28 11:09:49 +0800

    注释了线程池的案例 报线程泄露的问题 单独运行main 可以

--- wangdong/src/main/java/wangdong/MyThreadTest.java
+++ wangdong/src/main/java/wangdong/MyThreadTest.java
@@ -6,9 +6,13 @@ import java.util.concurrent.Executors;
 
 public class MyThreadTest {
 
+	/**
+	 * 直接运行这个方法 不要启动tomcat 不然会包线程泄露的问题 这只是一个单独的小例子啊!
+	 * 因为在主线程还没有结束的时候就执行了shutdown的操作所以会报线程泄漏
+	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		double stime=System.currentTimeMillis() ; 
+	/*	double stime=System.currentTimeMillis() ; 
 		ExecutorService exe = Executors.newFixedThreadPool(5);//固定连接的线程池
 		//ExecutorService exe=Executors.newCachedThreadPool();//可变大小的线程池
 		for (int i = 0; i < 8; i++) {
@@ -17,7 +21,7 @@ public class MyThreadTest {
 		}
 		exe.shutdown();
 		double etime=System.currentTimeMillis() ; 
-		System.out.println(etime-stime);
+		System.out.println(etime-stime); */
 	}
 
 }
--- wangdong/src/main/java/wangdong/SpringQtz.java
+++ wangdong/src/main/java/wangdong/SpringQtz.java
@@ -1,5 +1,8 @@
 package wangdong;
 
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLConnection;
 import java.sql.Date;
 
 public class SpringQtz {
@@ -9,5 +12,28 @@ public class SpringQtz {
 		long ms = System.currentTimeMillis();
 		System.out.println("\t\t" + new Date(ms));
 		System.out.println("(" + counter++ + ")");
+		
+		  try {
+				URL url=new URL("http://www.net.cn/domain/taodomaindetail?spm=5334.652006.4.4.z1c657&domainName=renrenhuimai.com");
+			    URLConnection   conn=url.openConnection();
+			    HttpURLConnection htpconn=(HttpURLConnection)conn;
+			    int code =htpconn.getResponseCode();
+			    boolean flag=false;
+			    if(code==200)
+			    {
+			    	
+			    	flag=true;
+			    	System.out.println("访问成功");
+		           
+			    }else{
+			    	
+			    	flag=false;
+			    	System.out.println("访问失败");
+			    }
+			  
+			  } catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 	}
 }
